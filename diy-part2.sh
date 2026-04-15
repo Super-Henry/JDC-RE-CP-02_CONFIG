5a2f97b588991a078bd43c31746505a4c667dea3
Brave-Henry <h17191199328@outlook.com>
2026-04-15 18:07:47 +0800

    Update OpenWrt DIY script for version 22.03.3
    update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,18 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+openwrt_version="22.03.3"
+case $openwrt_version in
+    "22.03.3")
+        kernel_md5="2974fbe1fa59be88f13eb8abeac8c10b"
+        ;;
+    "22.03.2")
+        kernel_md5="c91e62db69d188afca1b6cc5c9e1b72d"
+        ;;
+    "21.02.5")
+        kernel_md5="24ba228d1c9cc0e5974cb09dca64f48e"
+        ;;
+esac
 
 echo "-----------------Modify default IP"
 sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
@@ -23,14 +35,19 @@ sed -i 's/OpenWrt/Luban/g' package/base-files/files/bin/config_generate
 
 grep timezone -n5 package/base-files/files/bin/config_generate
 
-# 更换腾讯源
+echo '-----------------默认开启wifi'
+sed -i '/disabled=1/d' package/kernel/mac80211/files/lib/wifi/mac80211.sh 
+grep "devidx}.htmode" -n5 package/kernel/mac80211/files/lib/wifi/mac80211.sh 
 
-echo "src/gz openwrt_core http://mirrors.cloud.tencent.com/lede/releases/22.03.3/targets/ramips/mt7621/packages" >> package/system/opkg/files/customfeeds.conf
-echo "src/gz openwrt_base http://mirrors.cloud.tencent.com/lede/releases/22.03.3/packages/mipsel_24kc/base" >> package/system/opkg/files/customfeeds.conf
-echo "src/gz openwrt_luci http://mirrors.cloud.tencent.com/lede/releases/22.03.3/packages/mipsel_24kc/luci" >> package/system/opkg/files/customfeeds.conf
-echo "src/gz openwrt_packages http://mirrors.cloud.tencent.com/lede/releases/22.03.3/packages/mipsel_24kc/packages" >> package/system/opkg/files/customfeeds.conf
-echo "src/gz openwrt_routing http://mirrors.cloud.tencent.com/lede/releases/22.03.3/packages/mipsel_24kc/routing" >> package/system/opkg/files/customfeeds.conf
-echo "src/gz openwrt_telephony http://mirrors.cloud.tencent.com/lede/releases/22.03.3/packages/mipsel_24kc/telephony" >> package/system/opkg/files/customfeeds.conf
+echo "'-----------------自定义软件源"
+#sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/openwrt#g' /etc/opkg/distfeeds.conf
+echo "src/gz openwrt_core http://mirrors.cloud.tencent.com/lede/releases/${openwrt_version}/targets/ramips/mt7621/packages" >> package/system/opkg/files/customfeeds.conf
+echo "src/gz openwrt_base http://mirrors.cloud.tencent.com/lede/releases/${openwrt_version}/packages/mipsel_24kc/base" >> package/system/opkg/files/customfeeds.conf
+echo "src/gz openwrt_luci http://mirrors.cloud.tencent.com/lede/releases/${openwrt_version}/packages/mipsel_24kc/luci" >> package/system/opkg/files/customfeeds.conf
+echo "src/gz openwrt_packages http://mirrors.cloud.tencent.com/lede/releases/${openwrt_version}/packages/mipsel_24kc/packages" >> package/system/opkg/files/customfeeds.conf
+echo "src/gz openwrt_routing http://mirrors.cloud.tencent.com/lede/releases/${openwrt_version}/packages/mipsel_24kc/routing" >> package/system/opkg/files/customfeeds.conf
+echo "src/gz openwrt_telephony http://mirrors.cloud.tencent.com/lede/releases/${openwrt_version}/packages/mipsel_24kc/telephony" >> package/system/opkg/files/customfeeds.conf
+cat package/system/opkg/files/customfeeds.conf
 
 echo "-----------------修改u-boot的ramips"
 sed -i 's/yuncore,ax820/jdcloud,luban/g' package/boot/uboot-envtools/files/ramips
@@ -38,19 +55,19 @@ sed -i 's/yuncore,ax820/jdcloud,luban/g' package/boot/uboot-envtools/files/ramip
 grep all5002 -n5 package/boot/uboot-envtools/files/ramips
 
 echo '-----------------载入 mt7621_jdcloud_luban.dts'
-curl --retry 3 -s --globoff "https://gist.githubusercontent.com/vki888/12f935f75833058dfd6df69cd16dde8c/raw/a49bc98f720f2a19b49e98da2ca289c015bbae26/openwrt-autocompile-luban.dts" -o target/linux/ramips/dts/mt7621_jdcloud_luban.dts
+curl --retry 3 -s --globoff "https://gist.githubusercontent.com/vki888/9311d2b0854849f2f8eb3d89e5fb099b/raw/4d07a9f5f43010492baecc196bc701279ed67b50/%255Bopenwrt%255Dmt7621_jdcloud_luban_2.dts" -o target/linux/ramips/dts/mt7621_jdcloud_luban.dts
 cat target/linux/ramips/dts/mt7621_jdcloud_luban.dts
 
 # fix2 + fix4.2
 echo '-----------------修补 mt7621.mk'
 grep adslr_g7 -n10 target/linux/ramips/image/mt7621.mk
-sed -i '/Device\/adslr_g7/i\define Device\/jdcloud_luban\n  \$(Device\/dsa-migration)\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 15808k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := luban\n  DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7915-firmware kmod-mt7915e kmod-sdhci-mt7620 uboot-envtools kmod-mmc kmod-mtk-hnat kmod-mtd-rw wpad-openssl\nendef\nTARGET_DEVICES += jdcloud_luban\n\n' target/linux/ramips/image/mt7621.mk
+sed -i '/Device\/adslr_g7/i\define Device\/jdcloud_luban\n  \$(Device\/dsa-migration)\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 15808k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := luban\n  DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7915e kmod-sdhci-mt7620 uboot-envtools kmod-mmc kmod-mtk-hnat kmod-mtd-rw wpad-openssl\nendef\nTARGET_DEVICES += jdcloud_luban\n\n' target/linux/ramips/image/mt7621.mk
 grep adslr_g7 -n10 target/linux/ramips/image/mt7621.mk
 
 # fix3 + fix5.2
 echo '-----------------修补 02-network'
-sed -i '/xiaoyu,xy-c5/i\jdcloud,luban|\\' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-grep xiaoyu,xy-c5 -n3 target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+sed -i '/xiaomi,redmi-router-ac2100/i\jdcloud,luban|\\' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+grep xiaomi,redmi-router-ac2100 -n3 target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 
 #失败的配置，备份
 #sed -i -e '/hiwifi,hc5962|\\/i\jdcloud,luban|\\' -e '/ramips_setup_macs/,/}/{/ampedwireless,ally-00x19k/i\jdcloud,luban)\n\t\t[ "$PHYNBR" -eq 0 \] && echo $label_mac > /sys${DEVPATH}/macaddress\n\t\t\[ "$PHYNBR" -eq 1 \] && macaddr_add $label_mac 0x800000 > /sys${DEVPATH}/macaddress\n\t\t;;
@@ -74,8 +91,8 @@ grep xiaoyu,xy-c5 -n3 target/linux/ramips/mt7621/base-files/etc/board.d/02_netwo
 #sed -i '/ethernet: ethernet@1e100000 {/i\ethsys: ethsys@1e000000 {\n\tcompatible = "mediatek,mt7621-ethsys",\n\t\t"syscon";\n\treg = <0x1e000000 0x1000>;\n\t#clock-cells = <1>;\n\t};\n\n'  ./target/linux/ramips/dts/mt7621.dtsi	
 
 echo '-----------------定义kernel MD5，与官网一致'
-echo '2974fbe1fa59be88f13eb8abeac8c10b' > ./.vermagic
+echo ${kernel_md5} > ./.vermagic
 cat .vermagic
 
-#sed -i 's/^\tgrep.*vermagic/\tcp -f \$(TOPDIR)\/\.vermagic \$(LINUX_DIR)\/\.vermagic/g' include/kernel-defaults.mk
+sed -i 's/^\tgrep.*vermagic/\tcp -f \$(TOPDIR)\/\.vermagic \$(LINUX_DIR)\/\.vermagic/g' include/kernel-defaults.mk
 grep vermagic -n5 include/kernel-defaults.mk
